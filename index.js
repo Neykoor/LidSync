af3265b84e3b16534fc428b027396b30a7e34d14
Neykoor <cafecafirexos@gmail.com>
2026-05-05 13:17:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,47 +4,17 @@ export function pluginLid(sock, options = {}) {
   const resolver = new LidResolver(sock, options);
 
   sock.lid = {
-    resolve: async (id) => {
-      try {
-        return await resolver.resolver(id);
-      } catch (error) {
-        return null;
-      }
-    },
-
+    resolve: async (id) => resolver.resolver(id).catch(() => null),
     resolveBatch: async (ids, opts) => {
-      if (!ids || typeof ids[Symbol.iterator] !== "function") {
-        return new Map();
-      }
-      try {
-        return await resolver.resolverLote(ids, opts);
-      } catch (error) {
-        return new Map();
-      }
+      if (!ids?.[Symbol.iterator]) return new Map();
+      return resolver.resolverLote(ids, opts).catch(() => new Map());
     },
-
-    resolveParticipants: async (participants) => {
-      try {
-        return await resolver.resolverParticipantes(participants);
-      } catch (error) {
-        return new Map();
-      }
-    },
-
-    preload: (pares) => {
-      if (pares) resolver.precargarCache(pares);
-    },
-
+    resolveParticipants: async (participants) => resolver.resolverParticipantes(participants).catch(() => new Map()),
+    preload: (pares) => pares && resolver.precargarCache(pares),
     getStats: () => resolver.getStats(),
-
     isResolvable: (id) => resolver.esResolvable(id),
-
-    syncStore: (forzar = false) => {
-      resolver.sincronizarDesdeStore(forzar);
-    },
-
+    syncStore: (forzar = false) => resolver.sincronizarDesdeStore(forzar),
     destroy: () => resolver.destroy(),
-
     onJoin: (callback) => resolver.onJoin(callback),
   };
 
